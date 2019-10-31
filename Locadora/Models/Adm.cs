b50581717b2920f644e450edee1637118ffe6676
Alexandre Jareck <alexandrejareck@gmail.com>
2019-10-30 21:06:03 -0300

    Classe Adm, Cliente e context criadas

---
+++ Locadora/Models/Adm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Adm
+    {
+        public Adm()
+        {
+            Login = "ADM";
+            Senha = "ADM";
+        }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ Locadora/Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    [Table("CLIENTE")]
+    public class Cliente
+    {
+        //public Cliente()
+        //{
+
+        //    Reserva = new List<Reserva>();
+
+        //    Pagamento = new List<Pagamento>();
+        //}
+        [Key]
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+        public string Cnh { get; set; }
+        public string CategoriaCnh { get; set; }
+        //public List<Reserva> Reserva { get; set; }        
+        //public List<Pagamento> Pagamento { get; set; }
+        public string PossuiReserva { get; set; }
+        public string Status { get; set; }
+        public int MotoId { get; set; }
+        public int CarroId { get; set; }
+    }
+}
