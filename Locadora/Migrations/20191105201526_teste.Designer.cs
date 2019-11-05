68b2bfee37bc0dd922d7600884fc800d558ac6f0
Lucas Fernandes <lucasfernandes_06@hotmail.com>
2019-11-05 18:26:02 -0300

    Formulário de cadastro do carro e da moto

--- Locadora/Migrations/20191105201526_teste.Designer.cs
+++ Locadora/Migrations/20191105201526_teste.Designer.cs
@@ -3,14 +3,16 @@ using Locadora.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Locadora.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20191105201526_teste")]
+    partial class teste
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
